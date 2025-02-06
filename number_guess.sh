45175d5a097d326ec26255360f0300a7526de977
EmiDBcode Leiva <emilianoleiva20j@gmail.com>
2025-02-06 22:32:39 +0000

    feat: Implement guessing game logic

--- number_guess.sh
+++ number_guess.sh
@@ -15,3 +15,41 @@ else
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYD games, and your best game took $BEST_GAME guesses."
   done
 fi
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    ((NUMBER_OF_GUESSES++))
+
+    if [[ $GUESS -lt $SECRET_NUMBER ]]
+    then
+      echo "It's higher than that, guess again:"
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
+    then
+      echo "It's lower than that, guess again:"
+    else
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+      USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+      echo "$USER_DATA" | while IFS="|" read GAMES_PLAYED BEST_GAME
+      do
+        if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
+        then
+          $PSQL "UPDATE users SET best_game=$NUMBER_OF_GUESSES WHERE username='$USERNAME'"
+        fi
+        ((GAMES_PLAYED++))
+        $PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE username='$USERNAME'"
+      done
+
+      break
+    fi
+  fi
+done
