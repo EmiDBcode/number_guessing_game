db7ec7dfc87c324e4ddde50df6f9432f3b86b4b0
EmiDBcode Leiva <emilianoleiva20j@gmail.com>
2025-02-06 22:30:18 +0000

    feat: Add username prompt and database integration

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+echo "Enter your username:"
+read USERNAME
+
+USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_DATA ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+else
+  echo "$USER_DATA" | while IFS="|" read USER_ID GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYD games, and your best game took $BEST_GAME guesses."
+  done
+fi
